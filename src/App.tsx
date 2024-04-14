311c7e719609e82d34c6636429451f6d161ff039
Amresh Kumar <41804653+AmKreta@users.noreply.github.com>
2024-04-14 17:37:27 +0530

    added themeContext as global context

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Feed from './page/feed/feed.component';
 import { fetchUsers } from './store/users/users.store';
 import { useDispatch } from 'react-redux';
+import { fetchPosts } from './store/posts/posts';
+import { ThemeMode } from '@lib/enum/theme.enum';
+import ThemeContext from '@lib/context/themeProvider';
 
 function App() {
+  const [ActiveTheme, setActiveTheme] = useState<ThemeMode>(ThemeMode.SYSTEM);
+
   const dispatch = useDispatch();
 
   useEffect(function(){
    dispatch(fetchUsers() as any);
+   dispatch(fetchPosts() as any);
   },[]);
 
   return (
-    <Feed />
+    <ThemeContext.Provider value={{mode:ActiveTheme, changeMode:setActiveTheme}}>
+      <Feed />
+    </ThemeContext.Provider>
   );
 }
 
