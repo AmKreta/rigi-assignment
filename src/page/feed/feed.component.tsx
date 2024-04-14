bea6cf0b7f7a34f33bdd9207a27a9eafdd230e42
Amresh Kumar <41804653+AmKreta@users.noreply.github.com>
2024-04-14 21:32:44 +0530

    added feepds page

--- src/page/feed/feed.component.tsx
+++ src/page/feed/feed.component.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store/store";
+import { Post as PostInterface } from "../../lib/types/types";
+import Post  from "../../components/post/post.component";
+import './feed.style.scss';
+import ThemeContext from "../../lib/context/themeProvider";
 
 const Feed: React.FC = function () {
-  return <h1>amk</h1>;
+  const {mode} = useContext(ThemeContext);
+  const posts:PostInterface[] = useSelector((state:RootState)=>state.post.list.data) as any as PostInterface[];
+  if(!posts.length){
+    return <div>loading</div>
+  }
+
+  return <div className={`${mode} feed-container`}>
+    {posts.map(post=><Post post={post} key={post.id} />)}
+  </div>
 };
 
 export default Feed;
