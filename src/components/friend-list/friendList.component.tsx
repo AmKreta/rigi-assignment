51d31c0aaf4c174063979b4a3af0a97ec7d0a65d
Amresh Kumar <41804653+AmKreta@users.noreply.github.com>
2024-04-16 18:45:56 +0530

    made app responsive

--- src/components/friend-list/friendList.component.tsx
+++ src/components/friend-list/friendList.component.tsx
@@ -1,4 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoClose } from "react-icons/io5";
 import ThemeContext from '../../lib/context/themeProvider';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../store/store';
@@ -8,16 +11,56 @@ import './friendList.style.scss';
 const FriendListComponent:React.FC = ()=>{
     const {mode:themeMode} = useContext(ThemeContext);
     const friends:User[] = useSelector((state:RootState)=>state.users.data);
+    const [showMembersInSmallScreen, setShowMembersInSmallScreen] = useState(false);
+    const [isSmallScreen, setIsSmallScreen] = useState(false);
 
-    return <div className={`${themeMode} friend-list-container`}>
-        <div className='friend-list-title'>Members</div>
-        {
-            friends.map(friend=><div key={friend.id} className='friend-list-item'>
+    function toggleShowInSmallScreen(){
+        setShowMembersInSmallScreen(prevState=>!prevState);
+    }
+
+    useEffect(function(){
+        function checkScreen(){
+            if(window.innerWidth<1100){
+                setIsSmallScreen(true);
+            }
+            else{
+                setIsSmallScreen(false);
+            }
+        }
+        window.addEventListener('resize', checkScreen);
+        checkScreen();
+        return ()=>{
+            window.removeEventListener('resize', checkScreen);
+        }
+    },[]);
+
+    return isSmallScreen
+        ?showMembersInSmallScreen
+            ?<>
+                {
+                    createPortal( <div className={`${themeMode} friend-list-container`}>
+                        <div className='friend-list-title'>Members</div>
+                        {
+                            friends.map(friend=><div key={friend.id} className='friend-list-item'>
+                            <img src={friend.profilePictureUrl} loading='lazy' alt='user-dp'/>
+                            <div>{friend.name}</div>
+                            </div>)
+                        }
+                        </div>, 
+                    document.body)
+                }
+                <IoClose color='var(--text1)' className='hamburger' onClick={toggleShowInSmallScreen}/>
+            </>
+            :<RxHamburgerMenu color='var(--text1)' className='hamburger' onClick={toggleShowInSmallScreen}/>
+        :<div className={`${themeMode} friend-list-container`}>
+            <div className='friend-list-title'>Members</div>
+            {
+                friends.map(friend=><div key={friend.id} className='friend-list-item'>
                 <img src={friend.profilePictureUrl} loading='lazy' alt='user-dp'/>
                 <div>{friend.name}</div>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>;
 }
 
 export default FriendListComponent;
