557f47e1d6eae4ae16a82d2331f88b012edd398f
Amresh Kumar <41804653+AmKreta@users.noreply.github.com>
2024-04-16 20:47:20 +0530

    added offline support
    offline support with network first , cache update strategy
    
    only serving from cache in offline mode

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -13,24 +13,6 @@ idbOpenRequest.onupgradeneeded = (e) =>{
   db.createObjectStore('post',{keyPath:'id'});
 }
 
-function addDataToPost(data){
-  console.log({post:data});
-  let postTransaction = db.transaction('post','readwrite');
-  const postStore = postTransaction.objectStore('post');
-  data.forEach((post)=>{
-    postStore.put(post);
-  });
-}
-
-function addDataToUser(data){
-  console.log({user:data});
-  let userTransaction = db.transaction('user','readwrite');
-  const postStore = userTransaction.objectStore('user');
-  data.forEach((user)=>{
-    postStore.put(user);
-  });
-}
-
 self.addEventListener('install',e=>{
 
 });
@@ -38,50 +20,142 @@ self.addEventListener('install',e=>{
 self.addEventListener('fetch', (event) => {
   const { request } = event;
   if (request.url.includes('rigi-react-assignment')) {
-    const fetchRequest = request.clone();
-    return fetch(fetchRequest)
-      .then(async (response) => {
-        let json = await response.json();
-        if(request.url.endsWith('users')){
-          addDataToUser(json);
-        }
-        else{
-          addDataToPost(json.data);
-        }
-        return response;
-      })
-      .catch(err=>{
-        console.log(err, request);
-        return err;
-    })
+    return navigator.onLine 
+      ? saveToIndexedDB(event)
+      : readFromIndexedDB(event);
   }
   else if(
     ['png','jpg','jpeg','gif','webp','svg','mp4','webm'].some(extension=>request.url.endsWith(extension))
     || ['githubusercontent','picsum'].some(word=>request.url.includes(word))
   ){
-    saveToCache();
-  }
-
-  function saveToCache(){
-    event.respondWith(
-      caches.match(request)
-        .then((response) => {
-          if (response) {
-            return response;
-          }
-          const fetchRequest = request.clone();
-          return fetch(fetchRequest).then((response) => {
-            if (!response) {
-              return response;
-            }
-            const responseToCache = response.clone();
-            caches.open(STATIC_CACHE)
-              .then((cache) => {
-                cache.put(request, responseToCache);
-              });
-            return response;
-          });
-        })
-    );
+    return navigator.onLine 
+      ?saveToCache(event)
+      :readFromCache(event);
   }
 });
+
+
+function readDataFromPost(page, limit){
+  return new Promise((resolve, reject)=>{
+    let postTransaction = db.transaction('post','readonly');
+    let postStore = postTransaction.objectStore('post');
+    let request = postStore.getAll();
+    request.onsuccess = (e)=>{
+      resolve({data: e.target.result});
+    }
+    request.onerror = e =>{
+      reject(e.target.error);
+    }
+  });
+}
+
+
+function addDataToPost(data){
+  let postTransaction = db.transaction('post','readwrite');
+  const postStore = postTransaction.objectStore('post');
+  data.forEach((post)=>{
+    postStore.put(post);
+  });
+}
+
+
+function readDataFromUser(){
+  return new Promise((resolve, reject)=>{
+    let userTransaction = db.transaction('user','readonly');
+    const userStore = userTransaction.objectStore('user');
+    const request = userStore.getAll();
+    request.onsuccess = (e)=>{
+      resolve(e.target.result);
+    }
+    request.onerror = e =>{
+      reject(e.target.eror);
+    }
+  });
+}
+
+
+function addDataToUser(data){
+  let userTransaction = db.transaction('user','readwrite');
+  const postStore = userTransaction.objectStore('user');
+  data.forEach((user)=>{
+    postStore.put(user);
+  });
+}
+
+
+function saveToCache(event){
+  const { request } = event;
+  const fetchRequest = request.clone();
+  event.respondWith(fetch(fetchRequest)
+    .then((response) => {
+      if (!response) {
+        return response;
+      }
+      const responseToCache = response.clone();
+      caches
+        .open(STATIC_CACHE)
+        .then((cache) => {
+          cache.put(request, responseToCache);
+        });
+      return response;
+  }));
+}
+
+
+function readFromCache(event){
+  const { request } = event;
+  event.respondWith(
+    caches
+      .match(request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+      })
+      .catch(err=>{
+        console.log('failed to read from cache', err);
+        return err;
+      })
+  )
+}
+
+
+function saveToIndexedDB(event){
+  const { request } = event;
+  const fetchRequest = request.clone();
+  return fetch(fetchRequest)
+    .then(async (response) => {
+      let json = await response.json();
+      if(request.url.endsWith('users')){
+        addDataToUser(json);
+      }
+      else{
+        addDataToPost(json.data);
+      }
+      return response;
+    })
+    .catch(err=>{
+      console.log(err, request);
+      return err;
+    });
+}
+
+
+function readFromIndexedDB(event){
+  const { request } = event;
+  if(request.url.endsWith('users')){
+    return event.respondWith(
+      readDataFromUser()
+      .then(res=> new Response(JSON.stringify(res)))
+    );
+  }
+  else{
+    const url = new URL(request.url);
+    const page = +url.searchParams.get('page');
+    const limit = +url.searchParams.get('limit');
+    return event.respondWith(
+      readDataFromPost(page, limit)
+      .then(res=>new Response(JSON.stringify(res)))
+    );
+  }
+}
