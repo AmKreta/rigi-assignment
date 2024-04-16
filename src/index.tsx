8d90a335d4e10d33f61e2588f8e11e7346bcde89
Amresh Kumar <41804653+AmKreta@users.noreply.github.com>
2024-04-16 18:18:16 +0530

    made sure service worker is installed before mounting app

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,15 @@ import router from './routes/routes';
 import "./index.css";
   
 
-const timer = setTimeout(() => {
+navigator.serviceWorker
+.register('serviceworker.js',{scope:'/'})
+.then((registration) => {
+  console.log('Service Worker registered with scope:', registration.scope);
+})
+.catch((err) => {
+  console.error('Service Worker registration failed:', err);
+})
+.finally(()=>{
   const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
   );
@@ -16,9 +24,6 @@ const timer = setTimeout(() => {
       <RouterProvider router={router} />
     </Provider>
   );
-
-  clearTimeout(timer);
-
   // If you want to start measuring performance in your app, pass a function
   // to log results (for example: reportWebVitals(console.log))
   // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
